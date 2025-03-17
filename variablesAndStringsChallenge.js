8fcd9036268f506df2e3b0ec40ca2cb94813441d
Te-Piha <t.niha@Outlook.com>
2025-03-17 22:19:56 +1300

    Update to make sense

--- variablesAndStringsChallenge.js
+++ variablesAndStringsChallenge.js
@@ -3,29 +3,29 @@
 console.log("Hello world")
 
 // 1️⃣ Create a variable labelled `myName` and assign it your name.
-
+let myName
 
 // 2️⃣ Create a variable labelled `greeting` that says "Hello, my name is ___" using your `myName` variable.
-
+let greeting
 
 // 3️⃣ Find the length of `greeting` and store it in a variable labelled `greetingLength`.
-
+let greetingLength
 
 // 4️⃣ Create a variable `favoriteFood` and assign it a string value.
-
-
+let favoriteFood
 // 5️⃣ Use string concatenation to create a sentence:
 // "My favorite food is ___ and I love eating it!"
+// and name the variable "foodSentence"
+let foodSentence
 
 
 // 6️⃣ Create a variable `uppercaseGreeting` that stores `greeting` in all uppercase.
-
+let uppercaseGreeting
 
 // 7️⃣ Create a variable `lowercaseFoodSentence` that stores `foodSentence` in all lowercase.
-
+let lowercaseFoodSentence
 
 
 
 // Export variables for testing (Do not edit this)
-export { myName, greeting, greetingLength, favoriteFood, foodSentence, uppercaseGreeting, lowercaseFoodSentence };
-
+export { myName, greeting, greetingLength, favoriteFood, foodSentence, uppercaseGreeting, lowercaseFoodSentence }
